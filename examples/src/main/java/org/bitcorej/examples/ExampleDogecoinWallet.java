777768467fb993798e4d0a0a2458a63010b8b478
Liu Pengpeng <liupppppp@gmail.com>
2019-05-09 00:25:24 +0800

    Add dogecoin & bch example

--- examples/src/main/java/org/bitcorej/examples/ExampleDogecoinWallet.java
+++ examples/src/main/java/org/bitcorej/examples/ExampleDogecoinWallet.java
@@ -1,4 +1,17 @@
 package org.bitcorej.examples;
 
+import org.bitcorej.chain.ChainState;
+import org.bitcorej.chain.ChainStateProxy;
+
+import java.util.Collections;
+import java.util.List;
+
 public class ExampleDogecoinWallet {
+    public static void main(String[] args) throws Exception {
+        ChainState doge = new ChainStateProxy("doge", "test");
+        System.out.println(doge.generateKeyPair("15ca31edea232edd1a168b5ac2e48fb7af5564d0e42921a28751edef5e6aab6c"));
+
+        List<String> keys = Collections.singletonList("15ca31edea232edd1a168b5ac2e48fb7af5564d0e42921a28751edef5e6aab6c");
+
+    }
 }
