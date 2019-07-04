3fd8e2e16fd12fcf4df3727d3ef156ed180d8aee
Liu Pengpeng <liupppppp@gmail.com>
2019-07-04 16:08:11 +0800

    add keypair generate for xrp

--- core/src/main/java/org/bitcorej/chain/ripple/RippleStateProvider.java
+++ core/src/main/java/org/bitcorej/chain/ripple/RippleStateProvider.java
@@ -5,12 +5,14 @@ import com.ripple.core.coretypes.Amount;
 import com.ripple.core.coretypes.uint.UInt32;
 import com.ripple.core.types.known.tx.signed.SignedTransaction;
 import com.ripple.core.types.known.tx.txns.Payment;
+import com.ripple.crypto.ecdsa.Seed;
 import org.bitcorej.chain.ChainState;
 import org.bitcorej.chain.KeyPair;
 import org.bitcorej.chain.Transaction;
 import org.json.JSONObject;
 
 import java.math.BigDecimal;
+import java.security.SecureRandom;
 import java.util.List;
 
 public class RippleStateProvider implements ChainState {
@@ -18,42 +20,18 @@ public class RippleStateProvider implements ChainState {
 
     private static final BigDecimal MAX_FEE = new BigDecimal("50").multiply(DECIMALS);
 
-//    private String createAddress(PublicKey pubKey) {
-//        byte[] pubKeyData = pubKey.getRaw();
-//
-//        SHA256Digest pubKeyInnerHash = new SHA256Digest();
-//        pubKeyInnerHash.update(pubKeyData, 0, pubKeyData.length);
-//        byte[] pubKeyInnerBytes = new byte[pubKeyInnerHash.getDigestSize()];
-//        pubKeyInnerHash.doFinal(pubKeyInnerBytes, 0);
-//
-//        RIPEMD160Digest pubKeyOuterHash = new RIPEMD160Digest();
-//        pubKeyOuterHash.update(pubKeyInnerBytes, 0, pubKeyInnerHash.getDigestSize());
-//        byte[] accountId = new byte[pubKeyOuterHash.getDigestSize()];
-//        pubKeyOuterHash.doFinal(accountId, 0);
-//
-//        byte[] addressTypePrefix = NumericUtil.hexToBytes("0x00");
-//        byte[] payload = ByteUtil.concat(addressTypePrefix, accountId);
-//        SHA256Digest checksum1 = new SHA256Digest();
-//        checksum1.update(payload, 0, payload.length);
-//        byte[] checksum1Bytes = new byte[checksum1.getDigestSize()];
-//        checksum1.doFinal(checksum1Bytes, 0);
-//
-//        SHA256Digest checksum2 = new SHA256Digest();
-//        checksum2.update(checksum1Bytes, 0, checksum1Bytes.length);
-//        byte[] out = new byte[checksum2.getDigestSize()];
-//        checksum2.doFinal(out, 0);
-//        byte[] checksum2Bytes = Arrays.copyOfRange(out, 0, 4);
-//        return Base58.encode(ByteUtil.concat(payload, checksum2Bytes));
-//    }
-
     @Override
     public KeyPair generateKeyPair(String secret) {
-        return null;
+        return new KeyPair(secret, AccountID.fromKeyPair(Seed.fromBase58(secret).keyPair()).toString());
     }
 
     @Override
     public KeyPair generateKeyPair() {
-        return null;
+        SecureRandom random = new SecureRandom();
+        byte[] seedBytes = new byte[16];
+        random.nextBytes(seedBytes);
+        Seed seed = new Seed(seedBytes);
+        return generateKeyPair(seed.toString());
     }
 
     @Override
@@ -70,7 +48,7 @@ public class RippleStateProvider implements ChainState {
         JSONObject amount = jsonObject.getJSONObject("Amount");
         tx.addInput(tx.new Input(jsonObject.getString("Account"), new BigDecimal(amount.getString("value"))));
         tx.addOutput(tx.new Output(jsonObject.getString("Destination"), new BigDecimal(amount.getString("value")), jsonObject.getString("DestinationTag")));
-        tx.setFee(new BigDecimal(jsonObject.getString("Fee")).divide(DECIMALS));
+        tx.setFee(new BigDecimal(jsonObject.getString("Fee")));
         return tx;
     }
 
@@ -83,7 +61,7 @@ public class RippleStateProvider implements ChainState {
         payment.as(AccountID.Account, jsonObject.getString("Account"));
         payment.as(AccountID.Destination, jsonObject.getString("Destination"));
         payment.as(UInt32.DestinationTag, jsonObject.getString("DestinationTag"));
-        payment.as(Amount.Amount, jsonObject.getJSONObject("Amount").getString("value"));
+        payment.as(Amount.Amount, new BigDecimal(jsonObject.getJSONObject("Amount").getString("value")).multiply(DECIMALS).toString());
         payment.as(UInt32.Sequence, jsonObject.getInt("Sequence"));
         payment.as(UInt32.LastLedgerSequence, jsonObject.getInt("LastLedgerSequence"));
         payment.as(Amount.Fee, jsonObject.getString("Fee"));
