7923e0300dff00a1c6d8b5a935441641dbcb9203
Liu Pengpeng <liupppppp@gmail.com>
2019-07-30 17:14:59 +0800

    add byte op util

--- core/src/main/java/org/bitcorej/utils/ByteUtil.java
+++ core/src/main/java/org/bitcorej/utils/ByteUtil.java
@@ -1,5 +1,16 @@
 package org.bitcorej.utils;
 
+import org.spongycastle.crypto.DataLengthException;
+import org.spongycastle.crypto.InvalidCipherTextException;
+import org.spongycastle.crypto.engines.AESFastEngine;
+import org.spongycastle.crypto.modes.CBCBlockCipher;
+import org.spongycastle.crypto.paddings.PaddedBufferedBlockCipher;
+import org.spongycastle.crypto.params.KeyParameter;
+import org.spongycastle.crypto.params.ParametersWithIV;
+
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 public class ByteUtil {
@@ -21,5 +32,44 @@ public class ByteUtil {
         byte[] result = Arrays.copyOf(b1, b1.length + b2.length);
         System.arraycopy(b2, 0, result, b1.length, b2.length);
         return result;
+    }
+
+    public static byte[] hexlify(String data){
+        ByteBuffer buffer = ByteBuffer.allocate(data.length());
+        for(char letter : data.toCharArray()){
+            buffer.put((byte) letter);
+        }
+        return buffer.array();
+    }
+
+    /**
+     * Function to encrypt a message with AES
+     * @param input data to encrypt
+     * @param key key for encryption
+     * @return AES Encription of input
+     */
+    public static byte[] encryptAES(byte[] input, byte[] key) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] result = md.digest(key);
+            byte[] ivBytes = new byte[16];
+            System.arraycopy(result, 32, ivBytes, 0, 16);
+            byte[] sksBytes = new byte[32];
+            System.arraycopy(result, 0, sksBytes, 0, 32);
+
+            PaddedBufferedBlockCipher cipher = new PaddedBufferedBlockCipher(new CBCBlockCipher(new AESFastEngine()));
+            cipher.init(true, new ParametersWithIV(new KeyParameter(sksBytes), ivBytes));
+            byte[] temp = new byte[input.length + (16 - (input.length % 16))];
+            System.arraycopy(input, 0, temp, 0, input.length);
+            Arrays.fill(temp, input.length, temp.length, (byte) (16 - (input.length % 16)));
+            byte[] out = new byte[cipher.getOutputSize(temp.length)];
+            int proc = cipher.processBytes(temp, 0, temp.length, out, 0);
+            cipher.doFinal(out, proc);
+            temp = new byte[out.length - 16];
+            System.arraycopy(out, 0, temp, 0, temp.length);
+            return temp;
+        } catch (NoSuchAlgorithmException | DataLengthException | IllegalStateException | InvalidCipherTextException ex) {
+        }
+        return null;
     }
 }
