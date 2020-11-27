4b7ef6ef3d1aa4d009d38adcd06bc390a36853eb
Liu Pengpeng <liupppppp@gmail.com>
2020-11-27 12:45:33 +0800

    add many params for utxo

--- core/src/main/java/org/bitcorej/chain/bitcoin/Recipient.java
+++ core/src/main/java/org/bitcorej/chain/bitcoin/Recipient.java
@@ -5,10 +5,19 @@ import java.math.BigDecimal;
 public class Recipient {
     private String address;
     private BigDecimal amount;
+    private long bip115BlockHeight;
+    private byte[] bip115BlockHash;
 
     public Recipient(String address, BigDecimal amount) {
         this.address = address;
         this.amount = amount;
+    }
+
+    public Recipient(String address, BigDecimal amount, long bip115BlockHeight, byte[] bip115BlockHash) {
+        this.address = address;
+        this.amount = amount;
+        this.bip115BlockHeight = bip115BlockHeight;
+        this.bip115BlockHash = bip115BlockHash;
     }
 
     public String getAddress() {
@@ -17,6 +26,14 @@ public class Recipient {
 
     public BigDecimal getAmount() {
         return amount;
+    }
+
+    public long getBip115BlockHeight() {
+        return bip115BlockHeight;
+    }
+
+    public byte[] getBip115BlockHash() {
+        return bip115BlockHash;
     }
 
     @Override
--- core/src/main/java/org/bitcorej/chain/bitcoin/UnspentOutput.java
+++ core/src/main/java/org/bitcorej/chain/bitcoin/UnspentOutput.java
@@ -8,6 +8,8 @@ public class UnspentOutput {
     private int vout;
     private String address;
     private BigDecimal amount;
+    private long bip115BlockHeight;
+    private byte[] bip115BlockHash;
 
     public UnspentOutput(String txId, int vout, String address, BigDecimal amount) {
         if (address.matches("^(bitcoincash:)?(q|p)[a-z0-9]{41}")) {
@@ -17,6 +19,18 @@ public class UnspentOutput {
         this.vout = vout;
         this.address = address;
         this.amount = amount;
+    }
+
+    public UnspentOutput(String txId, int vout, String address, BigDecimal amount, long bip115BlockHeight, byte[] bip115BlockHash) {
+        if (address.matches("^(bitcoincash:)?(q|p)[a-z0-9]{41}")) {
+            address = AddressConverter.toLegacyAddress(address);
+        }
+        this.txId = txId;
+        this.vout = vout;
+        this.address = address;
+        this.amount = amount;
+        this.bip115BlockHeight = bip115BlockHeight;
+        this.bip115BlockHash = bip115BlockHash;
     }
 
     public String getTxId() {
@@ -37,6 +51,14 @@ public class UnspentOutput {
 
     public BigDecimal getAmount() {
         return amount;
+    }
+
+    public long getBip115BlockHeight() {
+        return bip115BlockHeight;
+    }
+
+    public byte[] getBip115BlockHash() {
+        return bip115BlockHash;
     }
 
     @Override
